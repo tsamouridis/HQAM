651c19318701bdc772038c5b22267ef1ddea1ee3
Valia Pappa <valia.pp8@gmail.com>
2022-06-03 11:19:58 +0300

    Squashed commit of the following:
    commit 30856dee4aa2c057d6422fd97d0d0d82e27963f9
    Author: Valia Pappa <valia.pp8@gmail.com>
    Date:   Fri Jun 3 11:14:00 2022 +0300
    
        Fix neural network performance
    
    commit eea394650be504c28f952ec6d4eeacee46214f92
    Author: Valia Pappa <valia.pp8@gmail.com>
    Date:   Wed Jun 1 20:41:07 2022 +0300
    
        Add detection using NNs - WIP

---
+++ detection with neural networks/NN.m
@@ -0,0 +1,38 @@
+close all;clear all;
+
+%% Constellation Generation
+method = "rHQAM";
+dmin = 2;
+M = 128;
+
+if method == "rHQAM"
+    constellation = rHQAM(M, dmin);
+elseif method == "irHQAM"
+    constellation = irHQAM(M, dmin);
+end
+
+%% NN declaration
+net = patternnet;
+
+%% Training
+sampleSize = 10000;
+[net, t] = training(net, sampleSize, constellation, method);
+
+%% Sent, Received Signals
+SNR = 10;
+signalSize = 1000;
+
+sent = createRandomSignal(signalSize, method, M, dmin);
+received = awgn(sent, SNR);
+
+%% Detection with Neural Network
+input(1, :) = real(received);
+input(2, :) = imag(received);
+
+netOutput = net(input);
+[~, estimatedSentSignalIndices] = max(netOutput, [], 1);
+
+estimatedSignal = zeros(1, length(received));
+for i=1:length(received)
+    estimatedSignal(i) = constellation(estimatedSentSignalIndices(i));
+end
---
+++ detection with neural networks/training.m
@@ -0,0 +1,25 @@
+function [net, targetOutput] = training(net, sampleSize, constellation, method)
+    SNR = 5;
+    dmin = 2;
+    M = length(constellation);
+    
+    trainingSent = createRandomSignal(sampleSize, method, M, dmin);
+
+    received = awgn(trainingSent , SNR);
+
+    trainingInput(1, :) = real(received);
+    trainingInput(2, :) = imag(received);
+
+    mld = zeros(1, length(received));
+    for i=1:length(received)
+        mld(i) = MLD(constellation, received(i));
+    end
+
+    targetOutput = zeros(M, length(received));
+    for i=1:length(mld)  
+        index = find(constellation==mld(i));
+        targetOutput(index, i) = targetOutput(index) + 1;
+    end
+    
+    net = train(net, trainingInput, targetOutput);
+end
